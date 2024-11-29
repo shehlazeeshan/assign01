eef888c0123c4d387e01c385ac0faa7c28fe8758
Shehla Zeeshan <azasulabd@gmail.com>
2024-11-29 05:49:02 -0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Homepage = () => {
   return (
     <div className='text-[27px] text-sky-600 p-9 ml-40'>
-    <h1>hello world
-
+    <h1>
+      hello world
     </h1>
     </div>
   )
